2ca72579352df1e470cbda2f75b7c01f50c4fcb6
Kevin Lutzer <kevinlutzer@proton.me>
2024-10-17 23:18:58 -0400

    WIP

---
+++ src/api.rs
@@ -0,0 +1,67 @@
+use std::{
+    net::{IpAddr, SocketAddr},
+    sync::Arc,
+};
+
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::get};
+use diesel::PgConnection;
+use paho_mqtt::AsyncClient;
+use tokio::sync::Mutex;
+use tracing::{error, info};
+
+use crate::settings::Settings;
+
+#[derive(Clone)]
+struct AppState {
+    mqtt_client: Arc<Mutex<AsyncClient>>,
+    db: Arc<Mutex<PgConnection>>,
+}
+
+async fn default_handler() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Not Found")
+}
+
+async fn healthcheck_handler(State(app_state): State<AppState>) -> (StatusCode, &'static str) {
+    let mqtt_client_lock = app_state.mqtt_client.lock().await;
+    let db_lock = app_state.db.lock().await;
+
+    // Check that the MQTT client is connected
+    let err_msg = if !mqtt_client_lock.is_connected() {
+        "MQTT client is not connected to the broker"
+    // Verify that we can query the database
+    } else {
+        ""
+    };
+
+    // else if db_lock.batch_execute("SELECT 1").is_err() {
+    //     "Database couldn't be connected too"
+    // }
+    // else {
+    //     ""
+    // };
+
+    if err_msg != "" {
+        error!(err_msg);
+        return (StatusCode::INTERNAL_SERVER_ERROR, err_msg);
+    }
+
+    info!("Healthcheck passed");
+    (StatusCode::OK, "Ok")
+}
+
+pub async fn server_proc(
+    config: Arc<Settings>,
+    mqtt_client: Arc<Mutex<AsyncClient>>,
+    db: Arc<Mutex<PgConnection>>,
+) {
+    let addr = SocketAddr::new(IpAddr::V4(config.host), config.port);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    let app = axum::Router::new()
+        .route("/v1/healthcheck", get(healthcheck_handler))
+        .fallback(default_handler)
+        .with_state(AppState { mqtt_client, db });
+
+    info!("Listening on {}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
---
+++ src/mqtt.rs
@@ -0,0 +1,116 @@
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::sync::Mutex;
+use tracing::info;
+
+use diesel::pg::PgConnection;
+use diesel::{insert_into, prelude::*};
+use futures::stream::StreamExt;
+use paho_mqtt::{
+    self as mqtt, properties, AsyncClient, ConnectOptionsBuilder, PropertyCode, SubscribeOptions,
+    MQTT_VERSION_5,
+};
+use serde::Deserialize;
+
+use crate::schema::rem_status::dsl::*;
+
+#[derive(Deserialize)]
+struct REMStatus {
+    id: String,
+    #[serde(rename = "deviceId")]
+    device_id: String,
+    #[serde(rename = "uptime")]
+    up_time: i32,
+}
+
+// The topics to which we subscribe.
+const TOPICS: &[&str] = &["rem/data", "rem/status"];
+const QOS: &[i32] = &[1, 1];
+
+pub async fn mqtt_proc(
+    cli: Arc<Mutex<AsyncClient>>,
+    conn: Arc<Mutex<PgConnection>>,
+) -> Result<(), mqtt::Error> {
+    let mut cli_lock = cli.lock().await;
+
+    // Get message stream before connecting.
+    let strm = &mut cli_lock.get_stream(25);
+
+    // Define the set of options for the connection
+    let lwt = paho_mqtt::Message::new(
+        "test/lwt",
+        "[LWT] Async subscriber v5 lost connection",
+        paho_mqtt::QOS_1,
+    );
+
+    // Connect with MQTT v5 and a persistent server session (no clean start).
+    // For a persistent v5 session, we must set the Session Expiry Interval
+    // on the server. Here we set that requests will persist for an hour
+    // (3600sec) if the service disconnects or restarts.
+    let conn_opts = ConnectOptionsBuilder::with_mqtt_version(MQTT_VERSION_5)
+        .clean_start(false)
+        .properties(properties![PropertyCode::SessionExpiryInterval => 3600])
+        .will_message(lwt)
+        .finalize();
+
+    // Make the connection to the broker
+    cli_lock.connect(conn_opts).await?;
+
+    info!("Subscribing to topics: {:?}", TOPICS);
+    let sub_opts = vec![SubscribeOptions::with_retain_as_published(); TOPICS.len()];
+    cli_lock
+        .subscribe_many_with_options(TOPICS, QOS, &sub_opts, None)
+        .await?;
+
+    drop(cli_lock);
+
+    // Just loop on incoming messages.
+    info!("Waiting for messages...");
+
+    // Note that we're not providing a way to cleanly shut down and
+    // disconnect. Therefore, when you kill this app (with a ^C or
+    // whatever) the server will get an unexpected drop and then
+    // should emit the LWT message.
+
+    while let Some(msg_opt) = strm.next().await {
+        if let Some(msg) = msg_opt {
+            if msg.retained() {
+                info!("(R) ");
+            }
+            info!("{}", msg);
+
+            let mut mut_conn = conn.lock().await;
+
+            // Handle the message
+            if msg.topic() == "rem/status" {
+                let status: REMStatus = serde_json::from_slice(msg.payload()).unwrap();
+                info!(
+                    "Device ID: {}, Uptime: {}",
+                    status.device_id, status.up_time
+                );
+
+                insert_into(rem_status)
+                    .values((
+                        id.eq(status.id),
+                        device_id.eq(status.device_id),
+                        up_time.eq(status.up_time),
+                    ))
+                    .execute(&mut *mut_conn)
+                    .unwrap();
+            }
+        } else {
+            // A "None" means we were disconnected. Try to reconnect...
+            info!("Lost connection. Attempting reconnect.");
+
+            let mut cli_lock = cli.lock().await;
+            while let Err(err) = &cli_lock.reconnect().await {
+                info!("Error reconnecting: {}", err);
+                // For tokio use: tokio::time::delay_for()
+                async_std::task::sleep(Duration::from_millis(1000)).await;
+            }
+        }
+    }
+
+    // Explicit return type for the async block
+    Ok::<(), paho_mqtt::Error>(())
+}
